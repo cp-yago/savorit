3830b38c4c76faa6f48dde8ab6aef5ba199669fb
Yago Cunha <yago.cunha123@gmail.com>
2025-02-06 17:05:18 -0300

    add `Next.js` to project

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Profile() {
+  return (
+    <>
+      <h1>Hello from Astolfo and Sushi!</h1>
+      <img
+        src="https://i.imgur.com/XNt8yrI.jpeg"
+        alt="My cats Astolfo and Sushi"
+        width={500}
+      />
+    </>
+  );
+}
