0c0709dacf7f8b4883782bf37f1db646583ea4ea
Yago Cunha <yago.cunha123@gmail.com>
2025-02-06 21:16:39 -0300

    adjusting formating

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const Home: React.FC = () => {
   return (
-    <h1 className="text-3xl font-bold underline bg-mint-500">
-      Hello world!
-    </h1>
-  )
+    <h1 className="text-3xl font-bold underline bg-mint-500">Hello world!</h1>
+  );
 };
 
 export default Home;
