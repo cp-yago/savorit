e337e59c4c4b63095b98c80da7fe45fc1200f9bc
Yago Cunha <yago.cunha123@gmail.com>
2025-02-11 11:49:00 -0300

    fix eslint errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const Home: React.FC = () => {
   return (
     <>
       <h1>Hello Astolfo and Sushi!</h1>
-      <img
+      <Image
         src="https://i.imgur.com/XNt8yrI.jpeg"
         alt="My cats Astolfo and Sushi"
         width={500}
+        height={500}
       />
       <div className="mt-4">
         <Link href="/add/add-recipe">
