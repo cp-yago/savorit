b10129b7e525f8e4481341ac2b2183c1ec7674b9
Yago Cunha <yago.cunha123@gmail.com>
2025-02-11 14:46:57 -0300

    add icon to input component

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -2,17 +2,24 @@ import * as React from "react";
 
 import { cn } from "app/lib/utils";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+interface InputProps extends React.ComponentProps<"input"> {
+  icon?: React.ReactNode;
+}
+
+function Input({ className, type, icon, ...props }: InputProps) {
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground aria-invalid:outline-destructive/60 aria-invalid:ring-destructive/20 dark:aria-invalid:outline-destructive dark:aria-invalid:ring-destructive/50 ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 aria-invalid:outline-destructive/60 dark:aria-invalid:outline-destructive dark:aria-invalid:ring-destructive/40 aria-invalid:ring-destructive/20 aria-invalid:border-destructive/60 dark:aria-invalid:border-destructive flex h-9 w-full min-w-0 rounded-full border bg-white px-3 py-1 text-base shadow-xs transition-[color,box-shadow] file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-4 focus-visible:outline-1 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:focus-visible:ring-[3px] aria-invalid:focus-visible:outline-none md:text-sm dark:aria-invalid:focus-visible:ring-4",
-        className,
-      )}
-      {...props}
-    />
+    <div className="relative flex-auto">
+      {icon && <span className="flex items-center justify-center text-gray-500 pointer-events-none absolute inset-y-0 left-0 h-full w-10">{icon}</span>}
+      <input
+        type={type}
+        data-slot="input"
+        className={cn(
+          "pl-10 border-input flex h-9 w-full min-w-0 rounded-full border bg-white pr-3 py-1 text-base shadow-xs",
+          className,
+        )}
+        {...props}
+      />
+    </div>
   );
 }
 
--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,9 +1,32 @@
+import { Input } from "@/components/ui/input";
 import React from "react";
 
-const Home: React.FC = () => {
+const RecipeList: React.FC = () => {
   return (
-    <h1 className="text-3xl font-bold underline bg-mint-500">Hello world!</h1>
+    <div className="flex flex-col items-center gap-4">
+      <header className="flex flex-row justify-between w-full py-4 px-5">
+        <h1 className="text-2xl">Receitas</h1>
+        <button>Add Recipe</button>
+      </header>
+      <div>
+        <Input
+          type="text"
+          placeholder="Pesquisar receitas"
+          className="w-80"
+          icon={(
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          )}
+        />
+      </div>
+      <main>
+        <div>Receitas</div>
+      </main>
+    </div>
   );
 };
 
-export default Home;
+export default RecipeList;
+
+
