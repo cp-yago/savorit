d4d84cb4692f49873397520c23c0ec7574d8ac20
Yago Cunha <yago.cunha123@gmail.com>
2025-02-19 15:16:04 -0300

    refactor: use env var in getRecipe func

--- app/features/recipes/actions/recipes.ts
+++ app/features/recipes/actions/recipes.ts
@@ -6,40 +6,41 @@ import { db } from "@/infra/db";
 import { eq } from "drizzle-orm";
 
 export async function generateRecipe({ url, id }: { url: string; id: string }) {
-  return fetch(
-    `https://smuk7f1osf.execute-api.us-east-1.amazonaws.com/generate-recipe?url=${url}&id=${id}`,
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  });
+    return fetch(
+        `https://smuk7f1osf.execute-api.us-east-1.amazonaws.com/generate-recipe?url=${url}&id=${id}`,
+    ).then((response) => {
+        // if (!response.ok) {
+        //   throw new Error("Network response was not ok");
+        // }
+        console.log("response", response);
+        return response.json();
+    });
 }
 
 export async function insertRecipe(data: InsertRecipe) {
-  const [newRecipe] = await db.insert(recipesTable).values(data).returning();
-  if (newRecipe == null) throw new Error("Failed to create recipe");
-  generateRecipe({ url: newRecipe.sourceUrl, id: newRecipe.id });
-  redirect(`/recipes/${newRecipe.id}`);
-  return newRecipe;
+    const [newRecipe] = await db.insert(recipesTable).values(data).returning();
+    if (newRecipe == null) throw new Error("Failed to create recipe");
+    generateRecipe({ url: newRecipe.sourceUrl, id: newRecipe.id });
+    redirect(`/recipes/${newRecipe.id}`);
+    return newRecipe;
 }
 
 export async function updateRecipe(id: string, data: Partial<InsertRecipe>) {
-  const [updatedRecipe] = await db
-    .update(recipesTable)
-    .set(data)
-    .where(eq(recipesTable.id, id))
-    .returning();
-  if (updatedRecipe == null) throw new Error("Failed to update recipe");
-  return updatedRecipe;
+    const [updatedRecipe] = await db
+        .update(recipesTable)
+        .set(data)
+        .where(eq(recipesTable.id, id))
+        .returning();
+    if (updatedRecipe == null) throw new Error("Failed to update recipe");
+    return updatedRecipe;
 }
 
 export async function findRecipeById(id: string) {
-  "use cache";
-  const recipe = await db.query.recipesTable.findFirst({
-    where: eq(recipesTable.id, id),
-  });
-  console.log("recipe findRecipeById", recipe);
-  cacheTag("recipes", recipe.id);
-  return recipe;
+    "use cache";
+    const recipe = await db.query.recipesTable.findFirst({
+        where: eq(recipesTable.id, id),
+    });
+    console.log("recipe findRecipeById", recipe);
+    cacheTag("recipes", recipe.id);
+    return recipe;
 }
