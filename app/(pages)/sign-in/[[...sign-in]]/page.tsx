f287a9da5c05e5032ec6515697321d26ed9159f1
Yago Cunha <yago.cunha123@gmail.com>
2025-02-26 16:11:05 -0300

    fix build errors

--- app/(pages)/sign-in/[[...sign-in]]/page.tsx
+++ app/(pages)/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,14 @@
 import Logo from "@/components/Logo";
 import { SignIn } from "@clerk/nextjs";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center h-svh">
-      <Logo className="mb-4" />
-      <SignIn />
-    </div>
+    <Suspense fallback="Loading...">
+      <div className="flex flex-col items-center justify-center h-svh">
+        <Logo className="mb-4" />
+        <SignIn />
+      </div>
+    </Suspense>
   );
 }
