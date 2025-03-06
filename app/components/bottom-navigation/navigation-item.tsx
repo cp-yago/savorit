533f901d900f922a0ec17514d5018376e3114bdc
Yago Cunha <yago.cunha123@gmail.com>
2025-03-06 14:57:34 -0300

    prettier fixes

--- app/components/bottom-navigation/navigation-item.tsx
+++ app/components/bottom-navigation/navigation-item.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -15,14 +15,18 @@ export function NavigationButton({
   const pathname = usePathname();
 
   const isActive = (path: string): boolean => {
-    return pathname === path ||
+    return (
+      pathname === path ||
       pathname.startsWith(`${path}/`) ||
-      (path === "/recipes" && pathname === "/");
+      (path === "/recipes" && pathname === "/")
+    );
   };
 
   return (
     <div className="relative flex flex-col items-center justify-center">
-      <div className={`absolute -top-1 border h-2 w-2/5 bg-black rounded-xl mb-2 ${isActive(path) ? 'visible' : 'invisible'}`}></div>
+      <div
+        className={`absolute -top-1 border h-2 w-2/5 bg-black rounded-xl mb-2 ${isActive(path) ? "visible" : "invisible"}`}
+      ></div>
       <Link
         href={path}
         className="inline-flex flex-col items-center justify-center hover:bg-gray-50 dark:hover:bg-gray-800 text-sm"
@@ -32,5 +36,4 @@ export function NavigationButton({
       </Link>
     </div>
   );
-};
-
+}
