b9dfc44ba2a414e32c4000e4ef7a8a7ed6b9d860
Yago Cunha <yago.cunha123@gmail.com>
2025-03-06 11:43:08 -0300

    hero component

---
+++ app/(pages)/(landing)/components/hero.tsx
@@ -0,0 +1,38 @@
+import Mockup from './mockup';
+
+// import AppStoreButton from './AppStoreButton';
+// import PlayStoreButton from './PlayStoreButton';
+
+
+export const heroDetails = {
+  heading: '🔎 A melhor forma de encontrar e organizar suas receitas das redes sociais',
+  subheading: 'Salve, organize e aprimore receitas do Instagram com inteligência e praticidade.',
+  centerImageSrc: '/images/hero-mockup.webp',
+}
+
+export default function Hero() {
+  return (
+    <section
+      id="hero"
+      className="relative flex items-center justify-center pb-0 px-5"
+    >
+      <div className="absolute left-0 top-0 bottom-0 -z-10 w-full">
+        <div className="absolute inset-0 h-full w-full bg-hero-background bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:40px_40px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]">
+        </div>
+      </div>
+
+      <div className="absolute left-0 right-0 bottom-0 backdrop-blur-[2px] h-40 bg-gradient-to-b from-transparent via-[rgba(233,238,255,0.5)] to-[rgba(202,208,230,0.5)]">
+      </div>
+
+      <div className="text-center">
+        <h1 className="text-2xl md:text-2xl md:leading-tight font-bold text-foreground max-w-lg md:max-w-2xl mx-auto">{heroDetails.heading}</h1>
+        <p className="mt-4 text-foreground max-w-lg mx-auto">{heroDetails.subheading}</p>
+        <div className="mt-6 flex flex-col sm:flex-row items-center sm:gap-4 w-fit mx-auto">
+          {/* <AppStoreButton dark />
+                    <PlayStoreButton dark /> */}
+        </div>
+        <Mockup />
+      </div>
+    </section>
+  );
+};
--- app/(pages)/(landing)/page.tsx
+++ app/(pages)/(landing)/page.tsx
@@ -1,9 +1,11 @@
 import Logo from "@/components/Logo";
+import Hero from "./components/hero";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center  bg-red-100 p-4 min-h-dvh">
       <Logo className="mb-6" />
+      <Hero />
     </div>
   );
 }
