e6fb0f199bf0369cf82f36526bf758783981086f
Yago Cunha <yago.cunha123@gmail.com>
2025-03-10 14:16:44 -0300

    Remove background colors from landing and books pages

--- app/(pages)/books/page.tsx
+++ app/(pages)/books/page.tsx
@@ -9,7 +9,7 @@ export default async function BooksPage() {
   const books = userId ? await findBooksByUserId(userId) : [];
 
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    <div className="min-h-screen flex flex-col">
       <div className="w-full flex-1">
         <Header
           title="Livros"
