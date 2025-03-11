a0705e00147645b00ce99c766d42da430d62e4bc
Yago Cunha <yago.cunha123@gmail.com>
2025-03-10 21:20:29 -0300

    feat(ui): add empty state handling for books, recipes, and book details
    - Implement empty state messages for books, recipes, and book details pages
    - Create reusable AddFirstCard component for adding first items
    - Update global styles with hover-scale class

--- app/(pages)/recipes/components/recipe-grid.tsx
+++ app/(pages)/recipes/components/recipe-grid.tsx
@@ -1,3 +1,4 @@
+import AddFirstCard from "@/components/add-first-card";
 import { UiRecipe } from "@/features/recipes/types";
 import RecipeCard from "./recipe-card";
 
@@ -8,6 +9,17 @@ interface RecipeGridProps {
 export default function RecipeGrid({ recipes }: RecipeGridProps) {
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      {recipes.length === 0 && (
+        <>
+          <div className="col-span-full flex justify-center items-center h-full">
+            <p className="text-gray-500 text-md text-center mb-4">
+              Você ainda não possui receitas. Clique no botão ou card para criar
+              a sua primeira.
+            </p>
+          </div>
+          <AddFirstCard href="/recipes/new" title="Adicionar Receita" />
+        </>
+      )}
       {recipes.map((recipe, index) => (
         <RecipeCard
           id={recipe.id}
